6df29425b1e1b64b5f1cb6da38284385536ce54f
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2025-01-01 20:44:31 +0530

    done

--- practice2/index.js
+++ practice2/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const connection = require('./config/db');
-const userRouter = require('./routes/userRoute');
+const express = require("express");
+const connection = require("./config/db");
+const userRouter = require("./routes/userRoute");
 const server = express();
 const PORT = 8080;
 
 server.use(express.json());
-server.use('/user', userRouter)
+server.use("/user", userRouter);
 
-server.listen(PORT, async(req,res)=>{
+server.listen(PORT, async (req, res) => {
   try {
-     await connection
-     console.log(`server is running on port ${PORT}, and DB is connected.`)
+    await connection;
+    console.log(`server is running on port ${PORT}, and DB is connected.`);
   } catch (error) {
-      console.log(`server is getting error${error}.`)
+    console.log(`server is getting error${error}.`);
   }
-})
+});
--- practice2/routes/userRoute.js
+++ practice2/routes/userRoute.js
@@ -1,56 +1,49 @@
-const express = require('express');
-const userModel = require('../models/userModel');
+const express = require("express");
+const userModel = require("../models/userModel");
 const userRouter = express.Router();
 
+userRouter.get("/", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(501).json(error);
+  }
+});
 
-userRouter.get('/', async(req,res)=>{
-    try {
-        const users = await userModel.find()
-        res.status(200).json({users})
+userRouter.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
-    } catch (error) {
-        res.status(501).json(error)
+  try {
+    const user = new userModel({
+      username,
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).json(`you are register as user ${user}.`);
+  } catch (error) {
+    res.status(400).json(`you are not register as user ${error}.`);
+  }
+});
 
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.send(`user Not Found.`);
     }
-})
-
-
-userRouter.post('/register', async(req,res)=>{
-    const {username,email,password}= req.body;
-
-     try {
-        const user = new userModel({
-            username,
-            email,
-            password
-        })
-        await user.save()
-        res.status(201).json(`you are register as user ${user}.`)
-     } catch (error) {
-        res.status(400).json(`you are not register as user ${error}.`)
-     }
-})
-
-userRouter.post('/login',async(req,res)=>{
-    const {email,password} = req.body;
-    try {
-        const user = await userModel.findOne({email});
-        if(!user){
-            return res.send(`user Not Found.`)
-
-        }
-        if(user){if(password==user.password){
-             return res.status(200).json(`login done ${user}.`)
-        }
-        else{
-            return res.send(`incorrect password.`)
-        }
-
-        }
-
-    } catch (error) {
-        res.status(400).json(error)
+    if (user) {
+      if (password == user.password) {
+        return res.status(200).json(`login done ${user}.`);
+      } else {
+        return res.send(`incorrect password.`);
+      }
     }
-})
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = userRouter;
